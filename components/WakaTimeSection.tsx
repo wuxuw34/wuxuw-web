e81e5519ff970abffbbacc00d3d96a2c14e001d5
lijianfeng <ljfwbd@gmail.com>
2026-01-29 22:52:55 +0800

    fix: 需要修复bug

--- components/WakaTimeSection.tsx
+++ components/WakaTimeSection.tsx
@@ -1,31 +1,51 @@
 "use client";
 import Apis from "@/apis";
+import { secondsToTime } from "@/utils/date";
 import { useEffect, useState } from "react";
 
 export default function WakaTimeSection() {
   const [languages, setLanguages] = useState<WakaTimeLanguage[]>([]);
+  const [codingActivity, setCodingActivity] = useState<WakaTimeCodingActivity>(
+    {} as WakaTimeCodingActivity,
+  );
 
   useEffect(() => {
     Apis.wakatime.getLanguages().then((data) => {
       setLanguages(data.data);
     });
-  }, []);
-
-  useEffect(() => {
     Apis.wakatime.getCodingActivity().then((data) => {
       console.log(data);
+      setCodingActivity(data);
     });
   }, []);
 
   return (
-    <div className="card">
+    <div className="card flex flex-col gap-2">
+      <div>
+        <div className="flex flex-col">
+          <span>编码统计</span>
+          <span className="text-secondary text-xs">所有时间 · All Time</span>
+        </div>
+        <div className="flex flex-row">
+          <div className="mini-card flex flex-col items-center justify-center px-6 py-4">
+            <span>
+              {codingActivity?.data?.grand_total?.total_seconds
+                ? secondsToTime(
+                    codingActivity?.data?.grand_total?.total_seconds || 0,
+                  )
+                : "N/A"}
+            </span>
+            <span className="text-secondary text-sm">总时间</span>
+          </div>
+        </div>
+      </div>
       <div>
         <div>常用语言</div>
         <ul className="flex flex-col gap-2">
           {languages.slice(0, 4).map((language) => (
             <li
               key={language.name}
-              className="flex flex-col gap-2 card"
+              className="flex shadow flex-col gap-2 card"
             >
               <div className="flex flex-row gap-1 items-center">
                 <span
--- utils/date.ts
+++ utils/date.ts
@@ -8,3 +8,14 @@ export function getFirstDayOfYear(year?: number): Date {
   year = year || new Date().getFullYear();
   return new Date(year, 0, 1);
 }
+
+export function secondsToTime(seconds: number): string {
+  const day = Math.floor(seconds / 86400);
+  const hours = Math.floor((seconds % 86400) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  if(day > 0){
+    return `${day}天${hours}时`
+  }
+  return `${day}天${hours}时${minutes}分${s}秒`;
+}
