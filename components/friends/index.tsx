5e7ee68984b3bfd353c2b76bdc8e709e755312ae
lijianfeng <ljfwbd@gmail.com>
2026-02-07 18:18:20 +0800

    友链部分基本完成

--- components/friends/index.tsx
+++ components/friends/index.tsx
@@ -1,12 +1,20 @@
 "use client";
 import Apis from "@/apis";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FaLink } from "react-icons/fa";
 import Friend from "./friend";
 import { FaUserPlus } from "react-icons/fa";
+import { Model } from "../model";
+import { FaUpload } from "react-icons/fa6";
+import Input from "../input";
 
 export default function Friends() {
   const [friends, setFriends] = useState<Friend[]>([]);
+  const [isModelOpen, setIsModelOpen] = useState(false);
+  const toggleAddFriendModal = useCallback(() => {
+    console.log("toggleAddFriendModal");
+    setIsModelOpen((pre) => !pre);
+  }, [setIsModelOpen]);
 
   useEffect(() => {
     Apis.friends.getAllFriends().then((res) => {
@@ -24,7 +32,10 @@ export default function Friends() {
       </h1>
       <h2 className="text-secondary text-sm">欢迎来到我的友链页面！</h2>
       <div className="flex flex-row justify-center">
-        <button className="card cursor-pointer hover:bg-primary! flex items-center gap-2">
+        <button
+          className="card cursor-pointer hover:bg-primary! flex items-center gap-2"
+          onClick={toggleAddFriendModal}
+        >
           <FaUserPlus />
           申请友链
         </button>
@@ -37,6 +48,55 @@ export default function Friends() {
           />
         ))}
       </div>
+      <Model
+        open={isModelOpen}
+        onClose={() => {
+          setIsModelOpen(false);
+        }}
+      >
+        <div className="rounded-sm flex flex-col gap-4 min-w-[400px] bg-[#4b4746] shadow-lg px-6! py-4!">
+          <h1 className="text-lg flex flex-row gap-2 items-center">
+            <FaUserPlus />
+            添加友链
+          </h1>
+          <div className="flex flex-col gap-2">
+            <div className="gap-2 flex flex-col">
+              <span className="text-secondary text-sm">网站名称</span>
+              <Input
+                className=" w-full"
+                placeholder="请输入网站名称"
+              />
+            </div>
+            <div className="gap-2 flex flex-col">
+              <span className="text-secondary text-sm">网站地址</span>
+              <Input
+                className="w-full"
+                placeholder="https://example.com"
+              />
+            </div>
+            <div className="gap-2 flex flex-col">
+              <span className="text-secondary text-sm">图标</span>
+              <Input
+                className=" w-full"
+                placeholder="请输入网站名称"
+              />
+            </div>
+            <div className="gap-2 flex flex-col">
+              <span className="text-secondary text-sm">网站描述</span>
+              <Input
+                className="w-full"
+                placeholder="请输入网站描述"
+              />
+            </div>
+          </div>
+          <div className="flex flex-row justify-end">
+            <button className="card hover:bg-primary! cursor-pointer flex flex-row items-center gap-2">
+              <FaUpload />
+              提交
+            </button>
+          </div>
+        </div>
+      </Model>
     </div>
   );
 }
--- components/model/model.tsx
+++ components/model/model.tsx
@@ -1,5 +1,12 @@
 "use client";
-import { forwardRef } from "react";
+import {
+  forwardRef,
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { createPortal } from "react-dom";
 
 interface ModelProps {
@@ -14,13 +21,33 @@ interface ModelRef {
 
 const Model = forwardRef<ModelRef, ModelProps>(
   ({ children, open, onClose }) => {
+    // const [modelOpen, setModelOpen] = useState(open ?? false);
+    const handleClose = useCallback(() => {}, [onClose]);
+    const handleClickBackdrop = useCallback(
+      (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target !== e.currentTarget) return;
+        onClose?.();
+      },
+      [onClose],
+    );
 
-    
+    // useEffect(() => {
+    //   if (open !== modelOpen) {
+    //     const update = () => {
+    //       setModelOpen(open ?? false);
+    //     };
+    //     update();
+    //   }
+    // }, [open]);
 
     if (!open) return null;
 
     return createPortal(
-      <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-50">
+      <div
+        id="model"
+        className="fixed top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-50 backdrop-blur-xs"
+        onClick={handleClickBackdrop}
+      >
         {children}
       </div>,
       document.body,
